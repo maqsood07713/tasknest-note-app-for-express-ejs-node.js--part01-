940d955b89acceec5c4a7d2dcd2f6ca4e8978503
Maqsood Ahmed <maqsoodcreator@gmail.com>
2026-04-26 16:28:44 +0500

    Adding all the updated files.

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express'); 
+const app = express();
+const Path = require('path');
+const fs = require('fs');
+const { log } = require('console');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');  
+app.use(express.static(Path.join(__dirname,"public")));
+
+
+app.get('/', (req, res) => {
+    fs.readdir('./files', (err, files) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.render('index', { files: files });
+    });
+});
+
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('_')}.txt`, req.body.details, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.redirect('/');
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
